3091f6f431fca7efdfb4e5b07f876cc1e897a4f9
Marwin <m.pinedag2014@gmail.com>
2023-10-29 16:27:14 -0500

    🔧 [CHANGE] hmtlWebpackPlugin added to webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -24,4 +25,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "JS Portfolio",
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
