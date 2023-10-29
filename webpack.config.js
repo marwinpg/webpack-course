1ced31e49e0ea3236489aecb0dc029228560c581
Marwin <m.pinedag2014@gmail.com>
2023-10-29 16:02:01 -0500

    ➕ [ADD] babel-loader added to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,19 @@ module.exports = {
   resolve: {
     extensions: [".js"],
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
 };
